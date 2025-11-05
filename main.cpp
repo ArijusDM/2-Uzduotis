991f50cc3c19042a4c9e612fc8e9c222a4ddfb03
ArijusDM <linge.ariju@gmail.com>
2025-11-05 18:18:21 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -169,7 +169,10 @@ int main(){
         if(galutinis < 5.0) vargsiukai.push_back(s);
         else kietiakai.push_back(s);
     }
-
+#ifdef NAUDOTI_VECTOR
+vargsiukai.shrink_to_fit();
+kietiakai.shrink_to_fit();
+#endif // NAUDOTI_VECTOR
     auto strat1_end = high_resolution_clock::now();
     auto trukmeStrat1 = duration_cast<milliseconds>(strat1_end - strat1_start).count();
     if(trukmeStrat1 > 1000)
@@ -191,6 +194,10 @@ int main(){
         Grupe.erase(it, Grupe.end());
     }
 
+#ifdef NAUDOTI_VECTOR
+vargsiukai2.shrink_to_fit();
+Grupe.shrink_to_fit();
+#endif // NAUDOTI_VECTOR
     auto strat2_end = high_resolution_clock::now();
     auto trukmeStrat2 = duration_cast<milliseconds>(strat2_end - strat2_start).count();
     if(trukmeStrat2 > 1000)
