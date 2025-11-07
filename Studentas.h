1a5befb3bef06ca1667671cbb5502a736c9538c6
ArijusDM <linge.ariju@gmail.com>
2025-11-07 15:40:58 +0200

    Update Studentas.h

--- Studentas.h
+++ Studentas.h
@@ -10,9 +10,9 @@
 #define NAUDOTI_LIST
 
 #ifdef NAUDOTI_LIST
-    using Konteineris = std::list<Studentas>;
+    using Konteineris = std::list<class Studentas>;
 #else
-    using Konteineris = std::vector<Studentas>;
+    using Konteineris = std::vector<class Studentas>;
 #endif
 
 class Studentas{
@@ -28,24 +28,24 @@ private:
 public:
     Studentas() : egzaminas_(0), galVid_(0), galMed_(0) {}
     Studentas(std::istream& is) {readStudent(is);}
-    
+
     inline string vardas() const {return vardas_;}
     inline string pavarde() const {return pavarde_;}
     inline double galVid() const {return galVid_;}
     inline double galMed() const {return galMed_;}
     inline int egzaminas() const {return egzaminas_;}
     inline const vector<int>& nd() const {return nd_;}
-    
+
     inline void setVardas(const string& v) {vardas_ = v;}
     inline void setPavarde(const string& p) {pavarde_ = p;}
     inline void setEgzaminas(int e) {egzaminas_ = e;}
     inline void setNd(const vector<int>& n) {nd_ = n;}
     inline void addNd(int paz) {nd_.push_back(paz);}
-    
+
     double galBalas(double (*skaiciavimas)(vector<int>&) = median) const;
     std::istream& readStudent(std::istream& is);
     void skaiciuotiGalutinis();
-    
+
     friend std::ostream& operator<<(std::ostream& os, const Studentas& s);
 };
 
